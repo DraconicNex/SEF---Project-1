540b189033efad1b0b19d78aca58a3f96b20d81b
DraconicNex <ajt1996@gmail.com>
2017-04-14 15:35:58 +1000

    Delete GameInterface.java

--- MultiPlayerMonsterGame/Server/GameInterface.java
+++
@@ -1,6 +0,0 @@
-   import java.rmi.*;
- 
-   public interface GameInterface extends Remote {
-	   public String join() throws RemoteException;
-	   public boolean move(String Player, int x,int y) throws RemoteException;
-   }
