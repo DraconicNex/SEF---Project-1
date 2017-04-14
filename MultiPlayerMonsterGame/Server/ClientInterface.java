f2fd9c4618198d2ba925f1b67be2a66c7beed1c4
DraconicNex2 <s3540645@student.rmit.edu.au>
2017-04-14 23:08:16 +1000

    name change

--- MultiPlayerMonsterGame/Server/ClientInterface.java
+++ MultiPlayerMonsterGame/Server/ClientInterface.java
@@ -1,6 +1,6 @@
    import java.rmi.*;
  
-   public interface GameInterface extends Remote {
+   public interface ClientInterface extends Remote {
 	   public String join() throws RemoteException;
 	   public boolean move(String Player, int x,int y) throws RemoteException;
    }
